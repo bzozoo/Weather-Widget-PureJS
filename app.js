d815aec36a8eabbf451ed2159a1763856e69b9fa
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-07-11 17:18:54 +0200

    Default widget

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ const weather = {
   city: "London",
   country: "GB"
 };
+
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+
+displayWeather(){
+
+iconElement.innerHTML = `<img src="icons/${weather.iconId}.png">`;
+
+tempElement.innerHTML = `${weather.temperature.value} ° <span>C</span>`;
+
+descElement.innerHTML = weather.description;
+
+locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
